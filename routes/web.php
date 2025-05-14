19b558eaa962ff483b3a6b0150c07bbf9551f76d
Dashsyy <59986967+Dashsyy@users.noreply.github.com>
2025-05-14 14:36:14 +0000

    Apply automatic changes

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,5 @@ use Illuminate\Support\Facades\Route;
 // $model = App\Models\Seeker::query()->first()
 Route::get('/resume/{id}', [\App\Http\Controllers\ResumeController::class, '__invoke']);
 Route::get('/resume_one', function () {
-	return view('templates.resume_one');
+    return view('templates.resume_one');
 });
