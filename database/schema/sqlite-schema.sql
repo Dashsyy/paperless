458139c1fea9a0e0b84acb0a7420a6c7737e75ff
heng.sunhour <heng.sunhour@smart.com.kh>
2025-06-01 09:29:58 +0700

    Update - add dump schema

---
+++ database/schema/sqlite-schema.sql
@@ -0,0 +1,203 @@
+CREATE TABLE IF NOT EXISTS "migrations"(
+  "id" integer primary key autoincrement not null,
+  "migration" varchar not null,
+  "batch" integer not null
+);
+CREATE TABLE IF NOT EXISTS "users"(
+  "id" integer primary key autoincrement not null,
+  "name" varchar not null,
+  "email" varchar not null,
+  "email_verified_at" datetime,
+  "password" varchar not null,
+  "remember_token" varchar,
+  "created_at" datetime,
+  "updated_at" datetime
+);
+CREATE UNIQUE INDEX "users_email_unique" on "users"("email");
+CREATE TABLE IF NOT EXISTS "password_reset_tokens"(
+  "email" varchar not null,
+  "token" varchar not null,
+  "created_at" datetime,
+  primary key("email")
+);
+CREATE TABLE IF NOT EXISTS "sessions"(
+  "id" varchar not null,
+  "user_id" integer,
+  "ip_address" varchar,
+  "user_agent" text,
+  "payload" text not null,
+  "last_activity" integer not null,
+  primary key("id")
+);
+CREATE INDEX "sessions_user_id_index" on "sessions"("user_id");
+CREATE INDEX "sessions_last_activity_index" on "sessions"("last_activity");
+CREATE TABLE IF NOT EXISTS "cache"(
+  "key" varchar not null,
+  "value" text not null,
+  "expiration" integer not null,
+  primary key("key")
+);
+CREATE TABLE IF NOT EXISTS "cache_locks"(
+  "key" varchar not null,
+  "owner" varchar not null,
+  "expiration" integer not null,
+  primary key("key")
+);
+CREATE TABLE IF NOT EXISTS "jobs"(
+  "id" integer primary key autoincrement not null,
+  "queue" varchar not null,
+  "payload" text not null,
+  "attempts" integer not null,
+  "reserved_at" integer,
+  "available_at" integer not null,
+  "created_at" integer not null
+);
+CREATE INDEX "jobs_queue_index" on "jobs"("queue");
+CREATE TABLE IF NOT EXISTS "job_batches"(
+  "id" varchar not null,
+  "name" varchar not null,
+  "total_jobs" integer not null,
+  "pending_jobs" integer not null,
+  "failed_jobs" integer not null,
+  "failed_job_ids" text not null,
+  "options" text,
+  "cancelled_at" integer,
+  "created_at" integer not null,
+  "finished_at" integer,
+  primary key("id")
+);
+CREATE TABLE IF NOT EXISTS "failed_jobs"(
+  "id" integer primary key autoincrement not null,
+  "uuid" varchar not null,
+  "connection" text not null,
+  "queue" text not null,
+  "payload" text not null,
+  "exception" text not null,
+  "failed_at" datetime not null default CURRENT_TIMESTAMP
+);
+CREATE UNIQUE INDEX "failed_jobs_uuid_unique" on "failed_jobs"("uuid");
+CREATE TABLE IF NOT EXISTS "personal_access_tokens"(
+  "id" integer primary key autoincrement not null,
+  "tokenable_type" varchar not null,
+  "tokenable_id" integer not null,
+  "name" varchar not null,
+  "token" varchar not null,
+  "abilities" text,
+  "last_used_at" datetime,
+  "expires_at" datetime,
+  "created_at" datetime,
+  "updated_at" datetime
+);
+CREATE INDEX "personal_access_tokens_tokenable_type_tokenable_id_index" on "personal_access_tokens"(
+  "tokenable_type",
+  "tokenable_id"
+);
+CREATE UNIQUE INDEX "personal_access_tokens_token_unique" on "personal_access_tokens"(
+  "token"
+);
+CREATE TABLE IF NOT EXISTS "permissions"(
+  "id" integer primary key autoincrement not null,
+  "name" varchar not null,
+  "guard_name" varchar not null,
+  "created_at" datetime,
+  "updated_at" datetime
+);
+CREATE UNIQUE INDEX "permissions_name_guard_name_unique" on "permissions"(
+  "name",
+  "guard_name"
+);
+CREATE TABLE IF NOT EXISTS "roles"(
+  "id" integer primary key autoincrement not null,
+  "name" varchar not null,
+  "guard_name" varchar not null,
+  "created_at" datetime,
+  "updated_at" datetime
+);
+CREATE UNIQUE INDEX "roles_name_guard_name_unique" on "roles"(
+  "name",
+  "guard_name"
+);
+CREATE TABLE IF NOT EXISTS "model_has_permissions"(
+  "permission_id" integer not null,
+  "model_type" varchar not null,
+  "model_id" integer not null,
+  foreign key("permission_id") references "permissions"("id") on delete cascade,
+  primary key("permission_id", "model_id", "model_type")
+);
+CREATE INDEX "model_has_permissions_model_id_model_type_index" on "model_has_permissions"(
+  "model_id",
+  "model_type"
+);
+CREATE TABLE IF NOT EXISTS "model_has_roles"(
+  "role_id" integer not null,
+  "model_type" varchar not null,
+  "model_id" integer not null,
+  foreign key("role_id") references "roles"("id") on delete cascade,
+  primary key("role_id", "model_id", "model_type")
+);
+CREATE INDEX "model_has_roles_model_id_model_type_index" on "model_has_roles"(
+  "model_id",
+  "model_type"
+);
+CREATE TABLE IF NOT EXISTS "role_has_permissions"(
+  "permission_id" integer not null,
+  "role_id" integer not null,
+  foreign key("permission_id") references "permissions"("id") on delete cascade,
+  foreign key("role_id") references "roles"("id") on delete cascade,
+  primary key("permission_id", "role_id")
+);
+CREATE TABLE IF NOT EXISTS "jobseekers"(
+  "id" integer primary key autoincrement not null,
+  "seeker_id" integer not null,
+  "role" varchar not null,
+  "description" varchar not null,
+  "created_at" datetime,
+  "updated_at" datetime
+);
+CREATE TABLE IF NOT EXISTS "seekers"(
+  "id" integer primary key autoincrement not null,
+  "name" varchar not null,
+  "phone" varchar not null,
+  "email" varchar,
+  "address" varchar,
+  "created_at" datetime,
+  "updated_at" datetime
+);
+CREATE INDEX "seekers_phone_index" on "seekers"("phone");
+CREATE INDEX "seekers_name_index" on "seekers"("name");
+CREATE TABLE IF NOT EXISTS "seeker_roles"(
+  "id" integer primary key autoincrement not null,
+  "name" varchar not null,
+  "created_at" datetime,
+  "updated_at" datetime
+);
+CREATE INDEX "seeker_roles_id_name_index" on "seeker_roles"("id", "name");
+CREATE INDEX "seeker_roles_name_index" on "seeker_roles"("name");
+CREATE TABLE IF NOT EXISTS "work_histories"(
+  "id" integer primary key autoincrement not null,
+  "seeker_id" varchar not null,
+  "start_date" date,
+  "end_date" date,
+  "company_name" varchar,
+  "position" varchar,
+  "description" text,
+  "created_at" datetime,
+  "updated_at" datetime
+);
+CREATE INDEX "work_histories_company_name_index" on "work_histories"(
+  "company_name"
+);
+CREATE INDEX "work_histories_start_date_end_date_index" on "work_histories"(
+  "start_date",
+  "end_date"
+);
+
+INSERT INTO migrations VALUES(1,'0001_01_01_000000_create_users_table',1);
+INSERT INTO migrations VALUES(2,'0001_01_01_000001_create_cache_table',1);
+INSERT INTO migrations VALUES(3,'0001_01_01_000002_create_jobs_table',1);
+INSERT INTO migrations VALUES(4,'2025_05_02_030559_create_personal_access_tokens_table',1);
+INSERT INTO migrations VALUES(5,'2025_05_02_065114_create_permission_tables',1);
+INSERT INTO migrations VALUES(6,'2025_05_07_021004_create_jobseekers_table',1);
+INSERT INTO migrations VALUES(7,'2025_05_07_022050_create_seeker_table',1);
+INSERT INTO migrations VALUES(8,'2025_05_07_063027_create_seeker_roles',1);
+INSERT INTO migrations VALUES(9,'2025_05_09_092702_create_work_histories_table',1);
